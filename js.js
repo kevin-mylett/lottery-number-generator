4acc60235bb767886ae30f6ced214f7a46953e92
kevin-mylett <kevinmylett@sky.com>
2017-02-19 13:44:00 +0000

    Generate random number between 1-10

--- js.js
+++ js.js
@@ -1,5 +1,20 @@
-function randomNumber(lower, upper) {
-	var choices = upper - lower + 1;
-	var selection = Math.floor(Math.random() * 10 + lower);
-	console.log(selection);
+
+const button = document.querySelector('.generateBtn');
+button.addEventListener("click", randomNumber);
+
+function randomNumber() {
+
+	function generateNumber(lower, upper) {
+		var choices = upper - lower + 1;
+		var selection = Math.floor(Math.random() * 10 + lower);
+		return selection
+	}
+
+	document.querySelector('.numbers').innerHTML = generateNumber(1, 10);
 }
+
+
+
+
+
+
