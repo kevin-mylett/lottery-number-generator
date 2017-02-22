123568fe577770dfbb1c0a591d1683d5da165a3b
kevin-mylett <kevinmylett@sky.com>
2017-02-22 23:06:21 +0000

    Added for loop to create 6 random numbers

--- js.js
+++ js.js
@@ -6,11 +6,18 @@ function randomNumber() {
 
 	function generateNumber(lower, upper) {
 		var choices = upper - lower + 1;
-		var selection = Math.floor(Math.random() * 10 + lower);
-		return selection
+		var randomNumber = Math.floor(Math.random() * 10 + lower);
+		return randomNumber
 	}
 
-	document.querySelector('.numbers').innerHTML = generateNumber(1, 10);
+	var selectedNumbers = []
+
+	for (let i=0; i < 6; i++) {
+		selectedNumbers.push(generateNumber(1, 10));
+	}
+	/*Need to place each number in the array in it's own div or span element*/
+	/*Perform check to stop repeated numbers*/
+	document.querySelector('.numbers').innerHTML = selectedNumbers[0] + ' ' + selectedNumbers[1] + ' ' + selectedNumbers[2] + ' ' + selectedNumbers[3] + ' ' + selectedNumbers[4] + ' ' + selectedNumbers[5];
 }
 
 
