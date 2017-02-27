25a2b2ba630a5a7c1389f9579dc2d3ef5b60bdd8
kevin-mylett <kevinmylett@sky.com>
2017-02-27 15:13:32 +0000

    Prevented duplicate numbers in array using indexOf

--- js.js
+++ js.js
@@ -4,36 +4,22 @@ button.addEventListener("click", randomNumber);
 
 function randomNumber() {
 
-	function generateNumber(lower, upper) {
-		var choices = upper - lower + 1;
-		var randomNumber = Math.floor(Math.random() * 10 + lower);
-		return randomNumber
+	function generateNumber() {
+		return Math.floor(Math.random() * 59 + 1);
 	}
 
 	var selectedNumbers = [];
 	var i= 0;
 
 	while (selectedNumbers.length < 6) {
-  		var checkNumber = generateNumber(1, 10);
-  
-		  if (selectedNumbers.includes(checkNumber)) {
-		    var checkNumber = generateNumber(1, 10);
-		  } else {
-		    selectedNumbers.push(checkNumber);
+		var newNumber = generateNumber();
+
+		if (selectedNumbers.indexOf(newNumber) === -1) {
+			selectedNumbers.push(newNumber);
 		    i++;
-		  }
+		} 
 	}
-
-	/*Prevented repeats in selectedNumbers array using while loop - but not enitrely convinced this is the 'correct' way to do this. 
-	**Also need to check includes() support as ES6 feature. indexOf() possible alternative.*/
-
 
 	/*Need to place each number in the array in it's own div or span element*/
 	document.querySelector('.numbers').innerHTML = selectedNumbers[0] + ' ' + selectedNumbers[1] + ' ' + selectedNumbers[2] + ' ' + selectedNumbers[3] + ' ' + selectedNumbers[4] + ' ' + selectedNumbers[5];
 }
-
-
-
-
-
-
